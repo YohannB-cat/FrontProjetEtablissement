b7afb6c1bef820a73d46c590b187139ff808b610
FlavienGomila <flavien.gomila@gmail.com>
2020-03-27 18:07:49 +0100

    plopyplop

--- src/app/content/classe/all-classe/all-classe.component.ts
+++ src/app/content/classe/all-classe/all-classe.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClasseCreateDto } from "src/app/models/classe-create-dto";
+import { ClassesService } from "src/app/services/classe/classes.service";
+import { ResponseDto } from 'src/app/models/response-dto';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-all-classe',
@@ -7,9 +11,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllClasseComponent implements OnInit {
 
-  constructor() { }
+  allClasse = new Array<ClasseCreateDto>();
+
+  classe: ClasseCreateDto;
+
+  messageErreur = '';
+
+  constructor(private service: ClassesService) { }
 
   ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this.messageErreur='';
+    this.service.getAll().subscribe(
+      (ResponseDto) => {
+        console.log('debug responseDto : ', ResponseDto);
+        if (!ResponseDto.error) {
+          this.allClasse = ResponseDto.object;
+        }
+      }
+    );
+  }
+
+  delete(id: number) {
+    this.messageErreur='';
+    this.service.delete(id).subscribe(
+      ResponseDto => {
+        console.log('debug responseDto : ', ResponseDto);
+        if (!ResponseDto.error) {
+          this.allClasse = this.allClasse.filter(
+            element => element.id !== id
+          );
+        }
+        console.log('result after delete : ', this.allClasse);
+      }
+    );
+  }
+
+  search(id: number) {
+    this.messageErreur='';
+    this.service.getId(id).subscribe(
+      //SUCCESS
+      (ResponseDto) => {
+        console.log('debug reponseDto : ', ResponseDto);
+        if (!ResponseDto.error) {
+          this.classe = ResponseDto.object;
+        }
+      },
+      //FAIL
+      (error) => {
+        console.log('debug error', error);
+        this.messageErreur= "Il n'y a pas de classe avec l'identifiant "+id;
+      }
+    );
   }
 
 }
