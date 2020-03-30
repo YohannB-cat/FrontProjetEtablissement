9032c37e2a17f67677dff949c78e75e126214f72
ClaraCdt <clara.cadet@yahoo.fr>
2020-03-30 12:27:19 +0200

    Validation create etudiant

--- src/app/content/etudiant/create-etudiant/create-etudiant.component.ts
+++ src/app/content/etudiant/create-etudiant/create-etudiant.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EtudiantDto } from 'src/app/models/etudiant-dto';
 import { EtudiantsService } from 'src/app/services/etudiant/etudiants.service';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-etudiant',
@@ -9,13 +10,27 @@ import { EtudiantsService } from 'src/app/services/etudiant/etudiants.service';
 })
 export class CreateEtudiantComponent implements OnInit {
 
+  etudiantForm : FormGroup;
   etudiant = new EtudiantDto();
   messageValidation = '';
 
   constructor(private service: EtudiantsService) { }
 
   ngOnInit() {
+    this.etudiantForm = new FormGroup({
+      'nom' : new FormControl(this.etudiant.nom, Validators.required),
+      'prenom': new FormControl(this.etudiant.prenom, Validators.required),
+      'email': new FormControl(this.etudiant.email, Validators.required),
+      'cni': new FormControl(this.etudiant.cni, Validators.required),
+      'sexe': new FormControl(this.etudiant.sexe, Validators.required)
+    })
   }
+
+  get nom() { return this.etudiantForm.get('nom'); }
+  get prenom() { return this.etudiantForm.get('prenom'); }
+  get email() { return this.etudiantForm.get('email'); }
+  get cni() { return this.etudiantForm.get('cni'); }
+  get sexe() { return this.etudiantForm.get('sexe'); }
 
   save() {
     this.service.create(this.etudiant).subscribe(
